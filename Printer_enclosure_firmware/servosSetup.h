de607aeab5e6ec2f85e861e4c5f39f44c8620a3e
me25542 <156400125+me25542@users.noreply.github.com>
2024-10-17 22:42:54 -0600

    added up-to-date files via upload

--- Printer_enclosure_firmware/servosSetup.h
+++ Printer_enclosure_firmware/servosSetup.h
@@ -1,13 +1,18 @@
 /*
- * Writen by Dalen Hardy 2024
+ * Copyright © 2024 Dalen Hardy
  * 
- * This code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * In no event shall the author be liable for any claim, damages, or other liability, 
- * whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the code 
- * or the use or other dealings in the code.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * Except as contained in this notice, the name of Dalen Hardy shall not be used in advertising or otherwise
+ * to promote the sale, use or other dealings in this Software without prior written authorization from Dalen Hardy.
 */
 
 #ifndef SERVOSSETUP_H
